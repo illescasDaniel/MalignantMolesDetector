be1f07ca49409abb2d86fd5458e815fe9efe758f
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2024-11-10 12:29:22 +0100

    Initial Commit

---
+++ SkinCancerDetector/SkinCancerDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCancerDetectorApp.swift
+//  SkinCancerDetector
+//
+//  Created by Daniel Illescas Romero on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SkinCancerDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkinCancerDetectorTests/SkinCancerDetectorTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkinCancerDetectorTests.swift
+//  SkinCancerDetectorTests
+//
+//  Created by Daniel Illescas Romero on 10/11/24.
+//
+
+import Testing
+
+struct SkinCancerDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
